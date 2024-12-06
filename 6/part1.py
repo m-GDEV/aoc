ed64576f3d52a6a626a27d5ea7aed55fcc209276
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-12-06 11:31:50 -0500

    works now

--- 6/part1.py
+++ 6/part1.py
@@ -1,4 +1,5 @@
 # Part 1
+import line_profiler
 
 class bcolors:
     HEADER = '\033[95m'
@@ -11,9 +12,9 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# Methods
-
 endkey = "\t"
+
+# Methods
 def print_with_color(text, color, endk=endkey):
     print(f"{color}{text}{bcolors.ENDC}", end=endk)
 
@@ -29,6 +30,11 @@ def get_local():
 #.........
 ......#..."""
 
+def get_remote():
+    f = open("/home/musa/aoc/6/input", "r")
+    return f.read()
+
+@line_profiler.profile
 def make_grid(s):
     g = []
 
@@ -57,7 +63,7 @@ def print_grid(s):
                 print(val, end=endkey)
         print("\n")
 
-
+@line_profiler.profile
 def find_value_in_grid(grid, value):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -66,6 +72,17 @@ def find_value_in_grid(grid, value):
     
     return None
 
+@line_profiler.profile
+def find_occurences_of_value_in_grid(grid, value):
+    occurences = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == value:
+                occurences += 1
+    
+    return occurences
+
+@line_profiler.profile
 def find_gaurd_in_grid(grid):
     left = find_value_in_grid(grid, "<")
     right = find_value_in_grid(grid, ">")
@@ -81,9 +98,12 @@ def find_gaurd_in_grid(grid):
     if bottom and not left and not right and not top:
         return ("v", bottom)
     
+    if not left and not right and not top and not bottom:
+        return None
+
     raise Exception("why are there two gaurds?")
 
-
+@line_profiler.profile
 def rotate_guard_right_90_deg(guard):
     match guard:
         case "<":
@@ -95,7 +115,7 @@ def rotate_guard_right_90_deg(guard):
         case "v":
             return "<"
 
-
+@line_profiler.profile
 def move_guard_left(guard, grid):
     guard_dir = guard[0]
     if guard_dir != "<": raise Exception("Cannot move left when gaurd is not turned left")
@@ -107,13 +127,17 @@ def move_guard_left(guard, grid):
     # Guard is leaving, simply mark their disappearance
     if gpx - 1 < 0:
         set_grid_spot(grid, "X", gpx,gpy)
+        return None
     # Guard hits roadblock
     elif get_grid_spot(grid, gpx - 1,gpy) == "#":
         set_grid_spot(grid, rotate_guard_right_90_deg(guard[0]), gpx,gpy)
+        return (get_grid_spot(grid, gpx, gpy), (gpy, gpx))
     else:
         set_grid_spot(grid, "X", gpx,gpy)
         set_grid_spot(grid, "<", gpx - 1, gpy)
+        return ("<", (gpy, gpx - 1))
 
+@line_profiler.profile
 def move_guard_right(guard, grid):
     guard_dir = guard[0]
     if guard_dir != ">": raise Exception("Cannot move right when gaurd is not turned right")
@@ -128,13 +152,17 @@ def move_guard_right(guard, grid):
     # Guard is leaving, simply mark their disappearance
     if gpx + 1 > boundary_x:
         set_grid_spot(grid, "X", gpx,gpy)
+        return None
     # Guard hits roadblock
     elif get_grid_spot(grid, gpx + 1,gpy) == "#":
         set_grid_spot(grid, rotate_guard_right_90_deg(guard[0]), gpx,gpy)
+        return (get_grid_spot(grid, gpx, gpy), (gpy, gpx))
     else:
         set_grid_spot(grid, "X", gpx,gpy)
         set_grid_spot(grid, ">", gpx + 1,gpy)
+        return (">", (gpy, gpx + 1))
 
+@line_profiler.profile
 def move_guard_up(guard, grid):
     guard_dir = guard[0]
     if guard_dir != "^": raise Exception("Cannot move up when gaurd is not turned up")
@@ -149,13 +177,17 @@ def move_guard_up(guard, grid):
     # Guard is leaving, simply mark their disappearance
     if gpy - 1 < 0:
         set_grid_spot(grid, "X", gpx,gpy)
+        return None
     # Guard hits roadblock
     elif get_grid_spot(grid, gpx,gpy - 1) == "#":
         set_grid_spot(grid, rotate_guard_right_90_deg(guard[0]), gpx,gpy) 
+        return (get_grid_spot(grid, gpx, gpy), (gpy, gpx))
     else:
         set_grid_spot(grid, "X", gpx,gpy)
         set_grid_spot(grid, "^", gpx,gpy - 1)
+        return ("^", (gpy - 1, gpx))
 
+@line_profiler.profile
 def move_guard_down(guard, grid):
     guard_dir = guard[0]
     if guard_dir != "v": raise Exception("Cannot move down when gaurd is not turned down")
@@ -170,13 +202,17 @@ def move_guard_down(guard, grid):
     # Guard is leaving, simply mark their disappearance
     if gpy + 1 > boundary_y:
         set_grid_spot(grid, "X", gpx,gpy)
+        return None
     # Guard hits roadblock
     elif get_grid_spot(grid, gpx,gpy + 1) == "#":
         set_grid_spot(grid, rotate_guard_right_90_deg(guard[0]),gpx,gpy)
+        return (get_grid_spot(grid, gpx, gpy), (gpy, gpx))
     else:
         set_grid_spot(grid, "X",gpx,gpy)
-        set_grid_spot(grid, "^", gpx,gpy + 1)
+        set_grid_spot(grid, "v", gpx,gpy + 1)
+        return ("v", (gpy + 1, gpx))
 
+@line_profiler.profile
 def get_grid_spot(grid, x, y):
     boundary_x = len(grid[0]) - 1
     boundary_y = len(grid) - 1
@@ -188,36 +224,48 @@ def get_grid_spot(grid, x, y):
 
     return grid[y][x]
 
+@line_profiler.profile
 def set_grid_spot(grid, newvalue, x, y):
     get_grid_spot(grid, x, y)
     grid[y][x] = newvalue
-    
 
-def traverse_once_in_grid(grid):
-    guard = find_gaurd_in_grid(grid)
+@line_profiler.profile
+def traverse_once_in_grid(grid, guard):
+    # guard = find_gaurd_in_grid(grid)
 
     match guard[0]:
         case "<":
-            move_guard_left(guard, grid)
+            return move_guard_left(guard, grid)
         case ">":
-            move_guard_right(guard, grid)
+            return move_guard_right(guard, grid)
         case "^":
-            move_guard_up(guard, grid)
+            return move_guard_up(guard, grid)
         case "v":
-            move_guard_down(guard, grid)
+            return move_guard_down(guard, grid)
 
 # Logic
-f = get_local()
 
-s = f.split("\n")
+def main():
+    f = get_local()
 
-g = make_grid(s)
+    s = f.split("\n")
+    # del s[-1]
 
-print_grid(g)
+    g = make_grid(s)
 
-# print(f"^ at: {find_value_in_grid(g, "^")}")
+    print_grid(g)
 
-traverse_once_in_grid(g)
+    traverse_num = 0
+    guard = find_gaurd_in_grid(g)
+    while guard != None:
+        if traverse_num % 100 == 0:
+            print(f"Traversed {traverse_num} times")
+        guard = traverse_once_in_grid(g, guard)
+        traverse_num += 1
+        print()
+        print_grid(g)
 
-print()
-print_grid(g)
+    o = find_occurences_of_value_in_grid(g, "X")
+    print(f"Found X {o} times in grid")
+
+main()

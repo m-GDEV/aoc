f978a46203b7a50deb3417d1afa9ebc05d02a058
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-12-08 16:58:52 -0500

    done day 7

--- 7/part1.py
+++ 7/part1.py
@@ -1,4 +1,6 @@
 # Part 1
+import itertools
+import re
 
 class bcolors:
     HEADER = '\033[95m'
@@ -28,43 +30,97 @@ def get_local():
 21037: 9 7 18 13
 292: 11 6 16 20"""
 
+
 def get_remote():
-    f = open("/home/musa/aoc/6/input", "r")
+    f = open("./7/input", "r")
     return f.read()
 
 
+def eval_left_to_right(s):
+    n1 = re.match('(\\d+)', s)
+    n1 = n1.groups()[0]
+    total = int(n1)
+    s = s.removeprefix(str(n1))
+
+    op = "x"
+
+    while op != None:
+
+        op = re.match("(\\*|\\+|\\>)", s)
+        if op is None:
+            continue
+        op = op.groups()[0]
+        s = s.removeprefix(str(op))
+
+        n2 = re.match('(\\d+)', s)
+        n2 = n2.groups()[0]
+        s = s.removeprefix(str(n2))
+
+        if op == ">":
+            total = int(str(total) + n2)
+        else:
+            total = eval(str(total) + op + n2) 
+
+    return total
+
 # Logic
-
-math_ops = ["+","*"]
-
 def main():
-    f = get_local()
+
+    f = get_remote()
 
     s = f.split("\n")
-    #  del s[-1]
+    del s[-1]
 
+    # Gather Info
     info = []
     for i in s:
         sp = i.split(":")
         target = sp[0]
         nums = sp[1].split(" ")
         nums.pop(0)
-
         info.append((target, nums))
 
+
+    # Main stuff
+    final = 0
+    final_count = 0
+
+    # Iterate through all lines in input
     for i in info:
-        print(i)
+        completed = False 
 
+        target = int(i[0])
+        nums = i[1]
+        l = len(nums) - 1
 
+        # thrid operator is meant to be '||' but i use > 
+        combs = itertools.product('+*>', repeat=l)
+        ll = list(combs)
 
-    for i in info:
-        print(f"Target: {info[0]}")
+        # Iterate through all combinations of operators placed in between nums
+        for comb in ll:
+            if completed is True:
+                break
+            eval_str = ""
 
-        if len(info[1]) == 2:
-            for m in math_ops:
-                left = i[1][0]
-                right = i[1][1]
-                res = eval(left + m + right)
-                print(f"Operator: {m}, {res=}")
+            # Create string that inserts operators into numbers
+            for k in range(len(nums)):
+                eval_str += nums[k]
+                if k != len(nums) - 1:
+                    eval_str += comb[k]
+
+            # Evaluate expression left to right
+            res = eval_left_to_right(eval_str)
+            if res == target:
+                final += res
+                completed = True
+                final_count += 1
+                # print(f"Result is True: {target=} | {res=} = {eval_str}")
+                print(f"{final_count} True Expressions")
+            else:
+                pass
+                # print(f"Result is False: {target=} | {res=} = {eval_str}")
+    
+    print(final)
 
 main()
